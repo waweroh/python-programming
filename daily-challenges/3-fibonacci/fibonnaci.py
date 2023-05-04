8a8fe1eb0d2baf5013c55209d5352c270d89b37a
waweroh <scadmsez@gmail.com>
2023-05-04 20:47:05 +0300

    print fibonacci numbers between 0 and x

--- daily-challenges/3-fibonacci/fibonnaci.py
+++ daily-challenges/3-fibonacci/fibonnaci.py
@@ -1,16 +1,20 @@
-def fibonacci_nums(n):
+def fibonacci_nums(x):
+
     fibonacci_list =[0]
-    a=0
-    b=1
+    a = 0
+    b = 1
     
 
-    while b<=n:
+    while b<=x:
         fibonacci_list.append(b)
+        #a becomes b and b becomes a+b as the loop runs
         a,b = b, a+b
-        return fibonacci_list
-    # print ("fib:")
-    # n = int(input("Number (n):"))
-    fib_seq = fibonacci_nums(100)
-    # print (f"fibonacci numbers between 0 and {n} are:{fib_seq}")
-    print(fib_seq)
+    return fibonacci_list
+print(f"Find Fibonacci numbers between 0 and x")
+x = int(input("Number (x):"))
+fib_seq = fibonacci_nums(x)
+print(f"Fibonacci numbers between 0 and {x} are: {fib_seq}")
+
+# if __name__ == "__main__":
+#     fibonacci_nums(x)
     
