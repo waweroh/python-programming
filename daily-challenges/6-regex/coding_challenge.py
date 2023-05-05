b5971dd2b05692b95de4398e0770d6466a2e6d18
waweroh <scadmsez@gmail.com>
2023-05-05 13:16:08 +0300

    feat: extracting emails

--- daily-challenges/6-regex/coding_challenge.py
+++ daily-challenges/6-regex/coding_challenge.py
@@ -1,5 +1,6 @@
 import re 
 
+
 def extract_phone_numbers(string):
     
     pattern = r"\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}"
@@ -9,9 +10,15 @@ def extract_phone_numbers(string):
     phone_numbers = re.findall(pattern, string)
     return phone_numbers
 
+def extract_email_addresses(string):
+    pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    email_addresses =re.findall(pattern, string)
+    return email_addresses
+
 
 string = "Please contact info@example.com for assistance. Phone: (123) 456-7890 or (111) 222-3333"
 print(extract_phone_numbers(string))
+print(extract_email_addresses(string))
 
 
 
