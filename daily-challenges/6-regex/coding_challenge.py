7a1ed8c07a23b42ffc97b3154faccc40d5b5609f
waweroh <scadmsez@gmail.com>
2023-05-05 13:24:46 +0300

    feat: replace email feature

--- daily-challenges/6-regex/coding_challenge.py
+++ daily-challenges/6-regex/coding_challenge.py
@@ -15,10 +15,19 @@ def extract_email_addresses(string):
     email_addresses =re.findall(pattern, string)
     return email_addresses
 
+def replace_email_addresses(string, replacement):
+    pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    replaced_email = re.sub(pattern,replacement,string)
+    return replaced_email
+    
+    
+
+
 
 string = "Please contact info@example.com for assistance. Phone: (123) 456-7890 or (111) 222-3333"
 print(extract_phone_numbers(string))
 print(extract_email_addresses(string))
+print(replace_email_addresses(string, "rickyk@gmail.com"))
 
 
 
