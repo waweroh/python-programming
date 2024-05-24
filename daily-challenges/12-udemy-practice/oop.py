be8170e43f2c65324cc8f1203944bf470f26fdd7
waweroh <scadmsez@gmail.com>
2024-05-24 15:48:47 +0300

    creating instances/objects and calling them

--- daily-challenges/12-udemy-practice/oop.py
+++ daily-challenges/12-udemy-practice/oop.py
@@ -7,3 +7,31 @@ print(type(obj1)) #<class '__main__.BigObject'>(instantiation)
 # class can be instantiated into other instances(objects)
 
 #another class
+'''
+  constructor method --is called whenever we instantiate.
+  When we call init during instantiation we accept what is after
+  self as the parameter.
+  self --- When you create an object (also known as an instance) from 
+          a class, self refers to that specific instance every time the class is instantiated.
+
+'''
+class PlayerCharacter:
+
+  def __init__(self, name, age ): 
+    self.name = name #attributes
+    self.age = age
+  
+  def run(self):
+    print('run')
+    return 'done'
+
+player1 = PlayerCharacter('moses', 144)
+player2 = PlayerCharacter('stephen', 56)
+player2.attack = 50 # You can now assign methods to the instances
+
+print(player1.name)
+print(player2.age)
+print(player1.run())
+print(player2.attack)
+  
+  
