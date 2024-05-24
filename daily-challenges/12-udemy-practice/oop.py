cbc69487df177a648f51c50d7bc4bf0ca7a0c205
waweroh <scadmsez@gmail.com>
2024-05-24 16:13:46 +0300

    class object attributes

--- daily-challenges/12-udemy-practice/oop.py
+++ daily-challenges/12-udemy-practice/oop.py
@@ -16,22 +16,28 @@ print(type(obj1)) #<class '__main__.BigObject'>(instantiation)
 
 '''
 class PlayerCharacter:
+  #class object attribute -- it's not dynamic across instances 
+  membership = True
 
   def __init__(self, name, age ): 
-    self.name = name #attributes
-    self.age = age
+    if (self.membership):
+      self.name = name #attributes - dynamic pieces of data across instances
+      self.age = age
   
-  def run(self):
-    print('run')
-    return 'done'
+  def shout (self):
+    print(f'my name is {self.name}, age is {self.age}')
+    
 
 player1 = PlayerCharacter('moses', 144)
 player2 = PlayerCharacter('stephen', 56)
 player2.attack = 50 # You can now assign methods to the instances
 
 print(player1.name)
+print(player1.membership)
 print(player2.age)
-print(player1.run())
+print(player1.shout())
 print(player2.attack)
+
+
   
   
