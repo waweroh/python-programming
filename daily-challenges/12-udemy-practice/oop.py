eadb8ab434434f5fb596bec2abcb6ed1c58cd5a4
waweroh <scadmsez@gmail.com>
2024-05-24 15:12:20 +0300

    basic class and an instantiation of the class

---
+++ daily-challenges/12-udemy-practice/oop.py
@@ -0,0 +1,9 @@
+class BigObject:
+  pass
+print(type(BigObject)) #<class 'type'>
+obj1 = BigObject()
+print(type(obj1)) #<class '__main__.BigObject'>(instantiation)
+
+# class can be instantiated into other instances(objects)
+
+#another class
