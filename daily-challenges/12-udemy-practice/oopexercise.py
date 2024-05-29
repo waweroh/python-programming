d2e6e2a448cfde3d46e99cd04cd99f134c9dd042
waweroh <scadmsez@gmail.com>
2024-05-29 20:05:15 +0300

    OOP task

---
+++ daily-challenges/12-udemy-practice/oopexercise.py
@@ -0,0 +1,49 @@
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+#1 Add another Cat
+class George(Cat):
+    def sing(self,sounds):
+        return f'{sounds}'
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = []
+
+cat1 = Simon('simon', 10)
+cat2 = Sally('sally', 5)
+cat3 = George('george', 15)
+
+my_cats.append(cat1)
+my_cats.append(cat2)
+my_cats.append(cat3)
+
+#3 Instantiate the Pet class with all your cats (list of cats) using the variable my_pets
+
+my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+my_pets.walk()
