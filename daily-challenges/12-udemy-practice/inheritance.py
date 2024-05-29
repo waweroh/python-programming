af38f1ea277b0e8e6771b4c4d4552943077df469
waweroh <scadmsez@gmail.com>
2024-05-29 20:22:05 +0300

    refactor: multiple inheritance

--- daily-challenges/12-udemy-practice/inheritance.py
+++ daily-challenges/12-udemy-practice/inheritance.py
@@ -4,9 +4,11 @@ Inheritance - allows a class to inherit attributes and methods from another clas
               relationship between classes.
 
 '''
+class User: #PARENT CLASS
+  def sign_in(self):
+    print('logged in')
 
-
-class Grandmaster():# user class inherited in Grandmaster class
+class Grandmaster(User):# user class inherited in Grandmaster class
   def __init__(self, name, power):
     self.name = name
     self.power = power
@@ -14,14 +16,14 @@ class Grandmaster():# user class inherited in Grandmaster class
   def attack(self):
     print(f'attacking with a power of {self.power} ')
 
-class Shaolin_archer():# user class inherited in archer class
+class Shaolin_archer(User):# user class inherited in archer class
   def __init__(self, name, num_arrows):
     self.name = name
     self.num_arrows = num_arrows
 
   def attack(self):
     print(f'attacking with {self.num_arrows} arrows left')
-    
+
 # MULTIPLE INHERITANCE
 class Grand_specialist(Grandmaster, Shaolin_archer):
   def __init__(self, name, power, num_arrows):
@@ -31,4 +33,6 @@ class Grand_specialist(Grandmaster, Shaolin_archer):
 grand_spec1 = Grand_specialist('jorge',50,43)
 print(grand_spec1.num_arrows)
 print(grand_spec1.power)
+print(grand_spec1.sign_in())
+print(grand_spec1.attack())
 
