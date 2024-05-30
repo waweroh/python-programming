23cb809072738ee5d4f21e213e80397cde4e777c
waweroh <scadmsez@gmail.com>
2024-05-30 09:58:15 +0300

    reduce

--- daily-challenges/12-udemy-practice/functions.py
+++ daily-challenges/12-udemy-practice/functions.py
@@ -162,6 +162,32 @@ def only_odd(item):
 print(list(zip( d_list, my_list, your_list)))
 print(d_list)
 
+'''
+Reduce - functools.reduce(function, iterable[, initializer])
+
+Apply function of two arguments cumulatively to the items of sequence, from left to right, so as to 
+reduce the sequence to a single value. For example, reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]) calculates 
+((((1+2)+3)+4)+5). The left argument, x, is the accumulated value and the right argument, y, is the update
+value from the sequence. If the optional initializer is present, it is placed before the items of the sequence 
+in the calculation, and serves as a default when the sequence is empty. If initializer is not given and sequence 
+contains only one item, the first item is returned.
+'''
+from functools import reduce
+your_list = [3,5,7]
+
+def multiply_by_4(item):
+  return item*4
+
+def only_odd(item):
+  return item % 2 != 0
+
+def accumulator(acc, item):
+  print(acc, item)
+  return acc + item
+
+print(reduce(accumulator, your_list, 10 ))
+print(your_list)
+
 
 
 
