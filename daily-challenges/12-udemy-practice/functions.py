646946f5c69afd3f7b2f5777961000d21d2d69d3
waweroh <scadmsez@gmail.com>
2024-05-30 13:46:15 +0300

    exercise: map filter zip reduce

--- daily-challenges/12-udemy-practice/functions.py
+++ daily-challenges/12-udemy-practice/functions.py
@@ -210,5 +210,37 @@ a.sort(key=lambda pair: pair[1])
 print(a)
 print(your2_list)
 
+from functools import reduce
+
+#1 Capitalize all of the pet names and print the list
+my_pets = ['nisi', 'babi', 'titi', 'carla']
+def capital(item):
+  a = item.capitalize()
+  return a
+print(list(map(capital, my_pets)))
+
+  
+#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5,4,3,2,1]
+
+my_numbers.sort()
+print(list(zip(my_numbers, my_strings)))
+
+
+#3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+def pass_mark(item):
+  return item > 50
+    
+print(list(filter(pass_mark, scores)))
+
+
+#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+def accumulator(acc, item):
+  a = (acc + item)
+  return a
+
+print(reduce(accumulator,(my_numbers + scores)) )
 
 
