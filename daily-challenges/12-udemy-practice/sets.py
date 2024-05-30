61de00c88c4e325a7096777b056154026c8ae31d
waweroh <scadmsez@gmail.com>
2024-05-30 12:05:23 +0300

    set comprehension

--- daily-challenges/12-udemy-practice/sets.py
+++ daily-challenges/12-udemy-practice/sets.py
@@ -36,4 +36,30 @@ print(my_set | your_set)
 # methods subset and superset
 
 
+'''
+list comprehensions
+    it is a short hand form used instead of using normal for loop and append methods
+
+    syntax: my_list[param(mutable) for param in iterable]
+'''
+#instead of
+ 
+my_set = set()
+for char in 'hello':
+  my_set.add(char)
+
+print(my_set)
+
+#do list comprehension
+my_set = {char for char in 'hello there'}
+my_set1 = {num for num in range(0,100)}
+my_set2 = {num**2 for num in range(0,30)}
+my_set3 = {num**2 for num in range(0,50)if num % 2 ==0}
+
+
+print(my_list)
+print(my_set1)
+print(my_set2)
+print(my_set3)
+
 
