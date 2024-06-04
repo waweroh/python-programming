eeb375987aa851afe482840484666ba85da4e902
waweroh <scadmsez@gmail.com>
2024-06-04 09:08:53 +0300

    HOF returning other functions

--- daily-challenges/12-udemy-practice/HOF..py
+++ daily-challenges/12-udemy-practice/HOF..py
@@ -13,3 +13,19 @@ def hello(other_fun, name):
   other_fun(name) #here we are executing the function taken as a parameter
 hello(greet_louder,'Moses')
 hello(greet_softer,'Sophie')
+
+def hello(name):
+  print('hello has been executed')
+  def greet():
+    print('Hi Jenny')
+  def welcome():
+      print('you are welcome sana')
+  if name == "Jenny":
+     return greet
+  else:
+     return welcome
+new_func = hello('Jenny')
+new_func2 = hello('John')
+new_func()
+new_func2()
+  
