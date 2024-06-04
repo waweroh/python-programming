ef3050cbe488302cf3a6e2757941ef3f5bfbf144
waweroh <scadmsez@gmail.com>
2024-06-04 08:48:14 +0300

    HOF taking 2 functions as arguments

--- daily-challenges/12-udemy-practice/HOF..py
+++ daily-challenges/12-udemy-practice/HOF..py
@@ -3,9 +3,13 @@ functions that take other functions as arguments
 
 '''
 
-def greet():
-  print('Hi')
-def display(other_fun):
+def greet_louder(name):
+  print(f'Hi {name.upper()}')
+def greet_softer(name):
+  print(f'Hi {name.lower()}')
+
+def hello(other_fun, name):
   print('This is display() function')
-  other_fun() #here we are executing the function taken as a parameter
-display(greet)
+  other_fun(name) #here we are executing the function taken as a parameter
+hello(greet_louder,'Moses')
+hello(greet_softer,'Sophie')
