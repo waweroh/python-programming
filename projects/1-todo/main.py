12bd2adeae1b1673faef00c38a050a2707f7578c
waweroh <scadmsez@gmail.com>
2024-08-01 06:14:49 +0300

    find length

--- projects/1-todo/main.py
+++ projects/1-todo/main.py
@@ -1,1 +1,7 @@
-print('enter to do item: ')
+# user_prompt = 'enter to do item: '
+# user_text = input(user_prompt)
+# print (user_text)
+
+text = input('title:')
+length = len(text)
+print ('the length of the title is : ', length)
