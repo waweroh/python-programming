50248949bf11d706f43c840be7cef95f1e2a406d
waweroh <scadmsez@gmail.com>
2024-08-01 06:33:10 +0300

    using while loop and a list for storing items

--- projects/1-todo/main.py
+++ projects/1-todo/main.py
@@ -2,6 +2,14 @@
 # user_text = input(user_prompt)
 # print (user_text)
 
-text = input('title:')
-length = len(text)
-print ('the length of the title is : ', length)
+# text = input('enter Todo: ')
+# length = len(text)
+# print ('the length of the title is : ', length)
+
+user_prompt = 'enter to do item: '
+
+todos = [] #set empty list outside the loo[]
+while True:
+  todo = input(user_prompt)
+  todos.append(todo) #method
+  print (todos)
