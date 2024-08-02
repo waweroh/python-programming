4a1ad95262416d518c5cf0fb81c4d3bdc7dc36f6
waweroh <scadmsez@gmail.com>
2024-08-02 06:30:13 +0300

    add edit feature

--- projects/1-todo/main.py
+++ projects/1-todo/main.py
@@ -11,7 +11,7 @@
 todos = [] #set empty list outside the loop
 
 while True:
-  user_action = input('type add, show or exit: ')
+  user_action = input('type add, edit, show or exit: ')
   user_action = user_action.strip() #remove whitespaces
 
   match user_action:
@@ -22,6 +22,14 @@ while True:
       for item in todos:
         item = item.capitalize() #manipulate the item
         print(item) #print each to do in new line not list
+
+    case 'edit': #change input to num, access items on list, replace an item 
+      number = int(input('Number to edit: ')) #indexing num to edit
+      number -= 1
+      existing_todo = todos[number]
+      print (existing_todo) # display what to edit
+      new_todo = input('Enter new todo: ') #new input
+      todos[number] = new_todo #replace
     case 'exit':
       break
     case _:
