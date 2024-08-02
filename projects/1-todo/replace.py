cda1576a1b7c0e8a6e56aca0ace18a65239f6483
waweroh <scadmsez@gmail.com>
2024-08-02 10:03:33 +0300

    changing a string

---
+++ projects/1-todo/replace.py
@@ -0,0 +1,6 @@
+filenames = ['1.Raw Data.txt', '2.Report.txt', '3.Presentation.txt']
+for filename in filenames:
+  filename = filename.replace('.','-',1 )# change immutable (string)
+  print(filename)
+  
+#tuple immutable list
