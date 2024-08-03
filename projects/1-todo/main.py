5d24db5e46344948e3c3425eb8654a2f39f272c2
waweroh <scadmsez@gmail.com>
2024-08-03 11:18:04 +0300

    add completed feature

--- projects/1-todo/main.py
+++ projects/1-todo/main.py
@@ -11,7 +11,7 @@
 todos = [] #set empty list outside the loop
 
 while True:
-  user_action = input('type add, edit, show or exit: ')
+  user_action = input('type add, edit,completed, show or exit: ')
   user_action = user_action.strip() #remove whitespaces
 
   match user_action:
@@ -21,7 +21,9 @@ while True:
     case 'show'| 'display':
       for index,item in enumerate(todos):
         item = item.capitalize() #manipulate the item
-        print(index,'.',item) #print each to do in new line not list
+        # index += 1 
+        row = f"{index + 1}-{item}"
+        print(row) #print each to do in new line not list
 
     case 'edit': #change input to num, access items on list, replace an item 
       number = int(input('Number to edit: ')) #indexing num to edit
@@ -30,6 +32,14 @@ while True:
       print (existing_todo) # display what to edit
       new_todo = input('Enter new todo: ') #new input
       todos[number] = new_todo #replace
+    case 'completed':
+      number = int(input('completed this task: '))
+      # number -= 1 
+      done = todos[number - 1]
+      print(done)
+      todos.pop(number)
+      
+      
     case 'exit':
       break
     case _:
