19c678a4922c2f635aa98270b2e60e7e4697ee33
waweroh <scadmsez@gmail.com>
2024-08-16 11:01:29 +0300

    modify the extension

--- projects/1-todo/replace.py
+++ projects/1-todo/replace.py
@@ -1,6 +1,7 @@
 filenames = ['1.Raw Data.txt', '2.Report.txt', '3.Presentation.txt']
 for filename in filenames:
   filename = filename.replace('.','-',1 )# change immutable (string)
+  filename = filename.replace('txt', 'py', 1)
   print(filename)
   
 #tuple immutable list
