773a3232c9cae12ba2dbb18406637cd84b0880f0
waweroh <scadmsez@gmail.com>
2024-08-16 11:02:26 +0300

    add function

---
+++ projects/1-todo/function.py
@@ -0,0 +1,10 @@
+def greet():
+  message = 'hello'
+  new_message = message.capitalize()
+  print('Hellooo')
+  return new_message
+
+greeting = greet()
+print(greeting)
+
+# function must have a return value
