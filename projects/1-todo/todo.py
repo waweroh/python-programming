df4687a57c09d645c29a7cb34eb0a089858bd068
waweroh <scadmsez@gmail.com>
2024-08-16 12:53:30 +0300

    complete and exit feature

--- projects/1-todo/todo.py
+++ projects/1-todo/todo.py
@@ -1,6 +1,6 @@
 todos = []
 while True:
-  user_action = input('type add, show, complete or edit: ' )
+  user_action = input('type add, show, complete, edit or exit: ' )
 
 
   if 'add' in user_action:
@@ -9,12 +9,13 @@ while True:
     todos.append(todo)
     # print(todos)
 
-  if 'show' in user_action:
+  elif 'show' in user_action:
     for index, item in enumerate(todos):
       item = item.capitalize()
       row = f'{index + 1}-{item}'
       print (row)
-  if 'edit' in user_action:
+
+  elif 'edit' in user_action:
     number = int(input('Enter number to edit: '))
     number -= 1
 
@@ -24,3 +25,23 @@ while True:
     new_todo = input('Enter new todo: ')
     todos[number] = new_todo
     print(new_todo)
+  
+  elif 'complete' in user_action:
+    number = int(input('Enter number of task completed: '))
+    number -=1 #  get index the done task
+
+    done_todo = todos[number] #get done task by assigning variable the index
+    print(f'task completed: {done_todo}')
+
+    todos.pop(number) #remove it
+
+  elif 'exit' in user_action:
+    break
+  
+  else:
+    print('You entered an unknown command')
+
+print('bye G !')
+
+
+
