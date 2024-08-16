9eb33586c76a757cf2ba281d946394e791a5893d
waweroh <scadmsez@gmail.com>
2024-08-16 11:07:26 +0300

    add maximum temperature to fahrenheit

--- projects/1-todo/example.py
+++ projects/1-todo/example.py
@@ -5,8 +5,17 @@
 # user_choice = int(input("Enter the index of the item: "))
 # message = f"You chose {menu[user_choice]}"
 # print(message)
-filenames = ['doc.txt', 'report.txt', 'presentation.txt']
-texts = ['hello','hello','hello']
-for text, filename in zip(texts, filenames):
-  file = open(f'files/{filename}', 'w')
-  file.write(text)
+# filenames = ['doc.txt', 'report.txt', 'presentation.txt']
+# texts = ['hello','hello','hello']
+# for text, filename in zip(texts, filenames):
+#   file = open(f'files/{filename}', 'w')
+#   file.write(text)
+def get_maximum():
+    celsius = [14, 15.1, 12.3]
+    maximum = max(celsius)
+    return maximum
+    
+celsius = get_maximum()
+fahrenheit = celsius * 1.8 + 32
+ 
+print(fahrenheit)
