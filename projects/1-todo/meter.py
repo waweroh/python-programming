c55be71c7f70d2b432255edfec79d5e70fbda0f8
waweroh <scadmsez@gmail.com>
2024-08-23 17:26:33 +0300

    decoupling a function

--- projects/1-todo/meter.py
+++ projects/1-todo/meter.py
@@ -1,15 +1,21 @@
 feet_inches = input('enter feet and inches: ')
 
-def convert (feet_inches):
+def parse(feet_inches):
   part = feet_inches.split(' ')
-  feet = float(part[0])
-  inches = float(part[1]) 
+  feet = float (part[0])
+  inches = float (part[1])
+  return {'feet': feet, 'inches':inches}
 
+# result = parse(feet_inches)
+# print(result)
+
+def convert (feet, inches):
   meters = feet * 0.3048 + inches * 0.0254
   return meters
 
-result = (convert(feet_inches))
-print(result)
+parsed = parse(feet_inches)
+result = (convert(parsed['feet'], parsed['inches']))
+print(f"{parsed['feet']} feet and {parsed['inches']} inches is equal to {result} meters")
 
 if result < 1:
   print('kid is too small')
