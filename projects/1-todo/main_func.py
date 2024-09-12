3ce8596fd16ca5cb5d8d5730fbab15d95454031c
waweroh <scadmsez@gmail.com>
2024-09-12 15:25:02 +0300

    name of the file is main

--- projects/1-todo/main_func.py
+++ projects/1-todo/main_func.py
@@ -9,7 +9,7 @@ def write_todo(todos_arg,filepath=FILEPATH):
   with open(filepath, 'w') as file:
     file.writelines(todos_arg)
 
-print(__name__) # __main__
+print(__name__)  # __main__
 
 # this code will only be executed in the main file not where this code is imported to
 if __name__ == '__main__': 
