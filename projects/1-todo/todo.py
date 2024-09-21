020f468e72c5d5d8ee788a6f8a5ccc5b24206fef
waweroh <scadmsez@gmail.com>
2024-09-21 12:50:04 +0300

    startwith, try except, functions, import functions

--- projects/1-todo/todo.py
+++ projects/1-todo/todo.py
@@ -1,68 +1,93 @@
-# todos = [] the list that stores todo list
+# todos = []
+
+import func2
+
 while True:
   user_action = input('type add,show,edit,complete,exit: ')
 
-  if 'add' in user_action:
-    todo = input('enter todo task: ') + '\n'
+  # if 'add' in user_action:
+  #   todo = input('enter todo task: ') + '\n'
+  if user_action.startswith('add'):
+    todo = user_action[4:] + '\n'
 
-    with open('todo2.txt', 'r') as file:
-      todos = file.readlines()#opens the file in lines
+    # with open('todofile.txt', 'r') as file:
+    #   todos = file.readlines()
+    todos = func2.read_todos()
 
-    todos.append(todo)
+    todos.append(todo) 
 
-    with open('todo2.txt', 'w') as file:
-      file.writelines(todos)
-      
+    # with open('todofile.txt', 'w') as file:
+    #   file.writelines(todos)
+    func2.write_todo(todos)
+  
+  # elif 'show' in user_action:
+  elif user_action.startswith('show'):
 
-  elif 'show' in user_action:
-    with open('todo2.txt', 'r') as file:
-      todos = file.readlines()
+    # with open('todofile.txt', 'r') as file:
+    #   todos = file.readlines()
+    todos = func2.read_todos()
 
     for index,item in enumerate(todos):
+      index += 1
       item = item.capitalize()
       item = item.strip('\n')
-      row = f'{index + 1}-{item}' 
+      row = f'{index}-{item}'
       print(row)
   
-  elif 'edit' in user_action:
-    number = int(input('enter todo task number: '))
-    number -= 1
+  # elif 'edit' in user_action:
+  elif user_action.startswith('edit'):
+    try:
+      number = int(user_action[5:])
+      number -= 1
 
-    with open('todo2.txt', 'r') as file:
-      todos = file.readlines()
+      # with open('todofile.txt', 'r') as file:
+      #   todos = file.readlines()
+      todos = func2.read_todos()
 
-    changing_todo = todos[number]
-    print(changing_todo)
+      changing_todo = todos[number]
+      print(changing_todo)
 
-    new_todo = input('enter modified todo task number: ')
-    todos[number] = new_todo + '\n'
+      new_todo = input('enter modified todo: ')
+      todos[number] = new_todo + '\n'
 
-    with open ('todo2.txt', 'w') as file:
-      file.writelines(todos)
-    
-  
-  elif 'complete' in user_action:
-    number = int(input('enter the index of the completed task: '))
-    number -= 1
+      # with open('todofile.txt', 'w') as file:
+      #   file.writelines(todos)
+      func2.write_todo(todos)
+    except ValueError:
+      print('command not valid enter an index')
+      continue
+    except IndexError:
+      print('enter correct index')
+    continue
+  # elif 'complete' in user_action:
+  elif user_action.startswith('complete'):
+    try:
+      number = int(user_action[9:])
+      number -= 1
 
-    with open('todo2.txt', 'r') as file:
-      todos = file.readlines()
+      # with open('todofile.txt', 'r') as file:
+      #   todos = file.readlines()
+      todos = func2.read_todos()
 
-    completed_task = todos[number]
-    print(f'task completed: {completed_task}')
+      completed_todo = todos[number]
+      print(f'completed task:{completed_todo}')
 
-    todos.pop(number)
+      todos.pop(number)
 
-    with open('todo2.txt', 'w') as file:
-      file.writelines(todos)
+      # with open('todofile.txt', 'w') as file:
+      #   file.writelines(todos)
+      func2.write_todo(todos)
+    except ValueError:
+      print('command not valid enter an index')
+      continue
+    except IndexError:
+      print('enter correct index')
+    continue
 
-  elif 'exit' in user_action:
+  elif user_action.startswith('exit'):
     break
-
+  
   else:
-    print('You entered the wrong command')
+    print('enter a valid command')
 
-print('goodday G')
-
-
-
+print('thank you G')
