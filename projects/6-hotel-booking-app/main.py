16958861e3bfaef35f94a955c4f6ded81ab8e51d
waweroh <scadmsez@gmail.com>
2024-10-31 10:14:45 +0300

    add hotel and reservation class

---
+++ projects/6-hotel-booking-app/main.py
@@ -0,0 +1,15 @@
+import pandas
+
+df = pandas.read_csv('hotels.csv')
+
+class Hotel:
+  def book(self):
+    pass
+
+
+class ReservationTicket:
+  def generate(self):
+    pass
+
+#program main loop or instances of the classes
+print(df)
