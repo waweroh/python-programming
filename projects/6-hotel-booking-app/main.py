2599054db03df97e76fe52662c65d208d309d2e7
waweroh <scadmsez@gmail.com>
2024-10-31 13:59:19 +0300

    add book method

--- projects/6-hotel-booking-app/main.py
+++ projects/6-hotel-booking-app/main.py
@@ -1,15 +1,18 @@
 import pandas
 
-df = pandas.read_csv('hotels.csv')
+df = pandas.read_csv('hotels.csv', dtype={'id': str})
 
 class Hotel:
   def __init__(self, hotel_id):
     self.hotel_id = hotel_id
     
   def book(self):
-    pass
+    '''Book a hotel by changing its availability to NO'''
+    df.loc[df['id'] == self.hotel_id, 'available'] = 'no'
+    df.to_csv('hotels.csv', index=False)
 
   def available(self):
+    '''Checks whether hotel is available'''
     availability = df.loc[df['id'] == self.hotel_id, 'available'].squeeze()
     if availability == 'yes':
       return True
